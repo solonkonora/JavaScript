313d45417ba3fcc2b10d87c2e00be4f6569be63a
Nkwada Nora Solonko <79814497+solonkonora@users.noreply.github.com>
2023-09-15 02:45:52 +0100

    Add files via upload

---
+++ Operator.js
@@ -0,0 +1,11 @@
+  //Comparison with the Greater Than Operator
+  function testGreaterThan(val) {
+    if (val > 100) {  
+      return "Over 100";
+    }
+    if (val > 10) {  
+      return "Over 10";
+    }
+    return "10 or Under";
+  }
+  console.log(testGreaterThan(10));
