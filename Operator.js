2adf6dbea30ffb61f4ce076b17006f4ff495a565
Nkwada Nora Solonko <79814497+solonkonora@users.noreply.github.com>
2023-09-15 04:35:49 +0100

    Add files via upload
    updated

--- Operator.js
+++ Operator.js
@@ -33,3 +33,18 @@
   return result;
 }
 console.log(testElse(4));
+
+
+//else if statement
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+else if (val < 5) {
+    return "Smaller than 5";
+  }
+else{  
+  return "Between 5 and 10";
+}
+}
+console.log(testElseIf(7));
